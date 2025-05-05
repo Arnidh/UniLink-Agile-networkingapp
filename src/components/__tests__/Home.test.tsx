b4396c0884844fce3b4087526752ef586ab4ef66
Mehul Goyal <145589189+Arnidh@users.noreply.github.com>
2025-05-05 17:39:34 +0530

    Delete src/components/__tests__ directory

--- src/components/__tests__/Home.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import Home from '../Home'  
-
-test('renders welcome message', () => {
-  render(<Home />)
-  expect(screen.getByText(/Welcome to UniLink/i)).toBeInTheDocument()
-})
